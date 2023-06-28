dbb225a6879b97f9cbd981e62a04aafb370f7f07
Erkin IT <erkin.it@gmail.com>
2023-06-28 22:41:24 +0600

    Add home work 2

---
+++ HW4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+//Ввод первого числа
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+//Ввод второго числа
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+//Ввод третего числа
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int MaxNumber = a;
+
+if (b > a) 
+{
+    MaxNumber = b;
+}
+
+if (c > b) 
+{
+    MaxNumber = c;
+}
+
+Console.WriteLine($"Максимальное число: {MaxNumber}");
